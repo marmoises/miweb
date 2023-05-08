7c6e2d6df931c3518c9087a85c644396a58daf67
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-05-08 00:28:47 -0300

    Agregando datos y codigo

--- prueba.py
+++ prueba.py
@@ -1,7 +1,11 @@
 def sumar(a:int,b:int)->int:
     return a+b
 
+def restar(a:int,b:int)->int:
+    return a-b
+
 def main():
     print(f'Resultado de la Suma: {sumar(1,2)}')
+    print(f'Resultado de la Resta: {restar(1,2)}')
 
 main()
