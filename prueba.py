5c023c70247ce080ad336b5685391c5da526ce42
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-05-09 14:48:42 -0300

    Agregando calculadora en prueba.py

--- prueba.py
+++ prueba.py
@@ -1,15 +1,24 @@
-def sumar(a:int,b:int)->int:
-    return a+b
+def calculadora(a:int,b:int,op:str)->int:
+    
+    def sumar()->int:
+        return a+b
 
-def restar(a:int,b:int)->int:
-    return a-b
+    def restar()->int:
+        return a-b
 
-def mult(a:int, b:int)->int:
-    return a*b
+    def mult()->int:
+        return a*b
+    
+    if op == 'sumar':
+        return sumar
+    elif op == 'restar':
+        return restar
+    elif op == 'mult':
+        return mult
 
 def main():
-    print(f'Resultado de la Suma: {sumar(1,2)}')
-    print(f'Resultado de la Resta: {restar(1,2)}')
-    print(f'Resultado de la Multiplicacion: {mult(1,2)}')
+    print(f'Resultado de la Suma: {calculadora(1,2,"sumar")()}')
+    print(f'Resultado de la Resta: {calculadora(1,2,"restar")()}')
+    print(f'Resultado de la Multiplicacion: {calculadora(1,2,"mult")()}')
 
 main()
