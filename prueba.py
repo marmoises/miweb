feff6d6b390ba01c981285b9dd5ecb9ad74d2f56
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-05-12 20:13:08 -0300

    Agregando pot() y mensaje

--- prueba.py
+++ prueba.py
@@ -8,17 +8,24 @@ def calculadora(a:int,b:int,op:str)->int:
 
     def mult()->int:
         return a*b
-    
+
+    def pot()->int:
+        return a**b
+        
     if op == 'sumar':
         return sumar
     elif op == 'restar':
         return restar
     elif op == 'mult':
         return mult
+    elif op == 'pot':
+        return pot
 
 def main():
+    print(f'Resultados generales: ')
     print(f'Resultado de la Suma: {calculadora(1,2,"sumar")()}')
     print(f'Resultado de la Resta: {calculadora(1,2,"restar")()}')
     print(f'Resultado de la Multiplicacion: {calculadora(1,2,"mult")()}')
+    print(f'Resultado de la Potencia: {calculadora(2,2,"pot")()}')
 
 main()
