d95b00c932a3828a5bc1e8abca4b95c8b78fb11d
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-05-24 00:40:40 -0300

    Agregando op. obtener_resto

--- prueba.py
+++ prueba.py
@@ -17,7 +17,13 @@ def calculadora(a:int,b:int,op:str)->int:
             return a/b
         except ZeroDivisionError as e:
             print(f'Error: {e}')
-    
+
+    def obtener_resto()->int:
+        try:
+            return a%b
+        except Exception as e:
+            print(f'Error: {e}')
+
     if op == 'sumar':
         return sumar
     elif op == 'restar':
@@ -28,6 +34,8 @@ def calculadora(a:int,b:int,op:str)->int:
         return division
     elif op == 'pot':
         return pot
+    elif op == 'resto':
+        return obtener_resto
 
 if __name__ == '__main__':
     def main():
@@ -37,5 +45,6 @@ if __name__ == '__main__':
         print(f'Resultado de la Multiplicacion: {calculadora(1,2,"mult")()}')
         print(f'Resultado de la Division: {calculadora(1,2,"division")()}')
         print(f'Resultado de la Potencia: {calculadora(2,2,"pot")()}')
+        print(f'Resultado del Resto de la Division: {calculadora(10,2,"resto")()}')
 
     main()
