6943180b1da5f9dfeec6fd082d49da73fd6c181b
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-06-21 23:29:20 -0300

    archivo3.txt deleted, infinity_iterators.py modified

--- infinity_iterators.py
+++ infinity_iterators.py
@@ -1,7 +1,8 @@
 from itertools import count
+
 iterator_infinity = count(1)
 value_getted = None
 
-for num in range(21):
+for num in range(20):
   value_getted = next(iterator_infinity)
   print(f'Valor: {value_getted}')
