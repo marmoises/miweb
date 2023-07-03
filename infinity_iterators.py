60d74d8ebc177f558afaa8104378dc780a431f76
marmoises <131649407+marmoises@users.noreply.github.com>
2023-07-03 20:26:35 -0300

    file inifinity_iterators.py modified

--- infinity_iterators.py
+++ infinity_iterators.py
@@ -1,8 +1,8 @@
 from itertools import count
 
 iterator_infinity = count(1)
-value_getted = None
+getted_value = None
 
-for num in range(20):
+for num in range(30):
   value_getted = next(iterator_infinity)
   print(f'Valor: {value_getted}')
