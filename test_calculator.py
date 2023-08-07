cbe646efc8cfc4e39561d3a67f7451f8ec14b50b
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-08-07 18:51:50 -0300

    test:Add test code for calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+from calculator import calculadora
+
+print(f'Resultados: ')
+print(f'Resultado de la Suma: {calculadora(1,2,"sumar")()}')
+print(f'Resultado de la Resta: {calculadora(1,2,"restar")()}')
+print(f'Resultado de la Multiplicacion: {calculadora(1,2,"mult")()}')
+print(f'Resultado de la Division: {calculadora(1,2,"division")()}')
+print(f'Resultado de la Potencia: {calculadora(2,2,"pot")()}')
+print(f'Resultado del Resto de la Division: {calculadora(10,2,"resto")()}')
