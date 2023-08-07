c529fae7672d668c3dc01bbff2ebf62871b54db8
Lucas Martinez <lucasmmartinez23@gmail.com>
2023-08-07 18:17:59 -0300

    refactor:Remove test code

--- calculator.py
+++ calculator.py
@@ -36,15 +36,3 @@ def calculadora(a:int,b:int,op:str)->int:
         return pot
     elif op == 'resto':
         return obtener_resto
-
-if __name__ == '__main__':
-    def main():
-        print(f'Resultados: ')
-        print(f'Resultado de la Suma: {calculadora(1,2,"sumar")()}')
-        print(f'Resultado de la Resta: {calculadora(1,2,"restar")()}')
-        print(f'Resultado de la Multiplicacion: {calculadora(1,2,"mult")()}')
-        print(f'Resultado de la Division: {calculadora(1,2,"division")()}')
-        print(f'Resultado de la Potencia: {calculadora(2,2,"pot")()}')
-        print(f'Resultado del Resto de la Division: {calculadora(10,2,"resto")()}')
-
-    main()
